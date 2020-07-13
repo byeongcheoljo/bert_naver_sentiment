1b7026529f91be49efea3d188f9a5b0b33e02cd8
byeongcheoljo <max91128@naver.com>
2020-07-13 15:01:20 +0900

    TrainFile, TestFile Read pandas

--- BERT_Naver_Movie_Sentiment.py
+++ BERT_Naver_Movie_Sentiment.py
@@ -1,1 +1,19 @@
+import tensorflow as tf
+import torch
+
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from transformers import get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+import random
+import time
+import datetime
+
+train = pd.read_csv("ratings_train.txt", sep="\t") ## Train_FileName
+test = pd.read_csv("ratings_test.txt", sep="\t")   ## Test_FileName
 
